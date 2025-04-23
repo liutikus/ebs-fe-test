5437d1ec99a9924373949dc4c5e7b097f73e091a
liutikus <niclcodreanu@gmail.com>
2025-04-23 12:51:11 +0300

    style(ui): add select filter option

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -30,7 +30,27 @@ const HomePage = () => {
 
   return (
     <div className="m-10">
-  
+  <div className="flex gap-4 mb-4">
+  <select
+    value={filter}
+    onChange={e => setFilter(e.target.value)}
+    className="px-4 py-2 rounded-md border cursor-pointer border-[#ECE5F0] capitalize text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#362166] focus:border-[#362166]"
+  >
+    <option value="">All</option>
+    {categories.map(c => (
+      <option key={c}>{c}</option>
+    ))}
+  </select>
+
+  <select
+    value={sort}
+    onChange={e => setSort(e.target.value as any)}
+    className="px-4 py-2 rounded-md border border-[#ECE5F0] cursor-pointer text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#362166] focus:border-[#362166]"
+  >
+    <option value="asc">Price: Low to High</option>
+    <option value="desc">Price: High to Low</option>
+  </select>
+</div>
       
       <ProductsList products={products}/>
     </div>
