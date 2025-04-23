3ef163eb15721f64c52dca48f4e3b5219e90ab3a
liutikus <niclcodreanu@gmail.com>
2025-04-23 09:19:38 +0300

    chore: install react-router and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 function App() {
   return (
     <div>
-      <h1>Welcome to the Front-End Developer Test!</h1>
-      <p>We wish you the best of luck. Please make sure to read the README file for
-        instructions.</p>
     </div>
   )
 }
