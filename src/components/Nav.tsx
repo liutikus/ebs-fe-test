3e8d79f7a95d44d74ee2bba9e70f6a8337a8443f
liutikus <niclcodreanu@gmail.com>
2025-04-23 13:46:36 +0300

    style(nav): add items quantity in nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,21 @@
 import { CiShoppingCart } from "react-icons/ci";
 import { Link } from "react-router-dom";
+import { useCart } from "../hooks/useCart";
 
 
 const Nav = () => {
+
+ const {items} = useCart()
+
   return (
     <nav className="flex justify-between px-5 py-2 bg-[#ECE5F0] sm:px-15 shadow-xl">
       <Link to='/'
        className="text-2xl font-light text-[#362166] cursor-pointer transition-transform duration-100 ease-in-out hover:scale-110 ">
         ProdCat</Link>
-        <Link to='/cart'><CiShoppingCart className="text-4xl text-[#362166] cursor-pointer transition-transform duration-100 ease-in-out hover:scale-110"/></Link>
+        <Link to='/cart' className="transition-transform duration-100 ease-in-out hover:scale-110">
+        <CiShoppingCart className=" inline text-4xl text-[#362166] "/>
+        <p className="inline mx-1 font-bold bg-[#362166] rounded-full p-1 text-[#ECE5F0]">{items.length}</p>
+        </Link>
     </nav>
   )
 }
