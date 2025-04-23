0c2b3fafc7c270e2e4d149780e65bc5b5c23c8f3
liutikus <niclcodreanu@gmail.com>
2025-04-23 18:53:50 +0300

    deploy fixed(3)

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
     <BrowserRouter>
       <MainLayout>
       <Routes>
-            <Route path="/ebs-fe-test/" element={<HomePage />} />
-            <Route path="ebs-fe-test/cart" element={<CartPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
           </Routes>
       </MainLayout>
     
