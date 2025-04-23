dd093a62f0b60368e7e989d40ef4dde5af15314c
liutikus <niclcodreanu@gmail.com>
2025-04-23 13:24:04 +0300

    feat: add navigation between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import CartProvider from "./context/CartContext"
 import MainLayout from "./layout/MainLayout"
 import HomePage from "./pages/HomePage"
+import CartPage from "./pages/CartPage"
 
 
 function App() {
 
   return (
   <CartProvider>
-    <MainLayout>
-      <HomePage/>
-    </MainLayout>
+    <BrowserRouter>
+      <MainLayout>
+      <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+      </MainLayout>
+    
+    </BrowserRouter>
   </CartProvider>
   )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,14 @@
 import { CiShoppingCart } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 
 const Nav = () => {
   return (
     <nav className="flex justify-between px-5 py-2 bg-[#ECE5F0] sm:px-15 shadow-xl">
-      <p className="text-2xl font-light text-[#362166] cursor-pointer transition-transform duration-100 ease-in-out hover:scale-110 ">
-        ProdCat</p>
-        <CiShoppingCart className="text-4xl text-[#362166] cursor-pointer transition-transform duration-100 ease-in-out hover:scale-110"/>
+      <Link to='/'
+       className="text-2xl font-light text-[#362166] cursor-pointer transition-transform duration-100 ease-in-out hover:scale-110 ">
+        ProdCat</Link>
+        <Link to='/cart'><CiShoppingCart className="text-4xl text-[#362166] cursor-pointer transition-transform duration-100 ease-in-out hover:scale-110"/></Link>
     </nav>
   )
 }
